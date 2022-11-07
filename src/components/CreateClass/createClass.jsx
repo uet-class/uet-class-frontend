8aeb868d9c387bf51e6354b494802edc1e2c2a25
Minh Phạm Vũ <minh.pv@teko.vn>
2022-11-07 21:18:19 +0700

    Add create class modal

--- src/components/CreateClass/createClass.jsx
+++ src/components/CreateClass/createClass.jsx
@@ -1,12 +1,13 @@
-import {Box, Typography} from "@mui/material";
+import {Box, Button, Grid, TextField, Typography} from "@mui/material";
 import React from "react";
+import AddIcon from "@mui/icons-material/Add";
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 600,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -16,12 +17,78 @@ const style = {
 const CreateClass = () => {
     return (
         <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-                Tiêu đề cho tạo lớp
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                Đây là modal tạo lớp.
-            </Typography>
+            <form>
+                <Box
+                    component={"form"}
+                >
+                    <Typography
+                        className={"login-info"}
+                        fontSize={20}
+                        fontWeight={500}
+                    >
+                        Tên lớp
+                    </Typography>
+                    <TextField
+                        name="username"
+                        className={"input-rounded"}
+                        sx={{
+                            width: "100%",
+                        }}
+                    ></TextField>
+                    <Typography
+                        className={"login-info"}
+                        fontSize={20}
+                        fontWeight={500}
+                    >
+                        Mã lớp
+                    </Typography>
+                    <TextField
+                        name="username"
+                        className={"input-rounded"}
+                        sx={{
+                            width: "100%",
+                        }}
+                    ></TextField>
+                    <Typography
+                        className={"login-info"}
+                        fontSize={20}
+                        fontWeight={500}
+                    >
+                        Mô tả
+                    </Typography>
+                    <TextField
+                        name="username"
+                        className={"input-rounded"}
+                        sx={{
+                            width: "100%",
+                            paddingBottom: 3,
+                        }}
+                    ></TextField>
+                    <Grid
+                        display="flex"
+                        justifyContent="flex-end"
+                    >
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            sx={{
+                                color: "#015198",
+                                borderRadius: 4,
+                            }}
+                        >
+                            <AddIcon style={{ color: 'white' }} />
+                            <Typography
+                                paddingLeft={1}
+                                className={"sign-in"}
+                                fontSize={20}
+                                fontWeight={500}
+                            >
+                                Tạo lớp
+                            </Typography>
+                        </Button>
+                    </Grid>
+                </Box>
+            </form>
         </Box>
     )
 }
