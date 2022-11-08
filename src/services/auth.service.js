730c6eea211e1045b72f0a841b2d09bb25eab418
Minh Phạm Vũ <minh.pv@teko.vn>
2022-11-08 22:48:19 +0700

    Change env

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_URL;
+const API_URL = process.env.REACT_APP_SERVER_URL;
+console.log(API_URL)
 
 class AuthService {
     login(email, password) {
