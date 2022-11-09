cbc96dda834a34bdf15a8f3a6f80dc52a330b390
Minh Phạm Vũ <minh.pv@teko.vn>
2022-11-09 20:07:42 +0700

    Finish making responsive sign in and sign up pages

--- src/pages/SignUp/signUp.jsx
+++ src/pages/SignUp/signUp.jsx
@@ -1,8 +1,15 @@
-import {createTheme, Grid, ThemeProvider} from "@mui/material";
-import ClassIllustration from "../../components/ClassIllustration/classIllustration";
+import {
+    Grid,
+    createTheme,
+    ThemeProvider,
+    CssBaseline,
+} from "@mui/material";
+import React from "react";
+import Classroom from "../../assets/classroom.png";
 import SignUpForm from "../../components/SignUpForm/signUpForm";
 
-const SignUp = () => {
+
+const SignIn = () => {
     const theme = createTheme({
         typography: {
             fontFamily: ["Inter", "sans-serif"].join(","),
@@ -11,11 +18,20 @@ const SignUp = () => {
 
     return (
         <ThemeProvider theme={theme}>
-            <Grid container justifyContent="center">
-                <Grid className={"classroom-illustration"} item xs={7.5}>
-                    <ClassIllustration />
-                </Grid>
-                <Grid className={"login-section"} item xs={4.5}>
+            <Grid container component="main" sx={{height: '100vh'}}>
+                <CssBaseline/>
+                <Grid
+                    item
+                    xs={false}
+                    sm={4}
+                    md={8}
+                    sx={{
+                        backgroundImage: `url(${Classroom})`,
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center',
+                    }}
+                />
+                <Grid item xs={12} sm={8} md={4} elevation={6} square>
                     <SignUpForm />
                 </Grid>
             </Grid>
@@ -23,4 +39,4 @@ const SignUp = () => {
     )
 }
 
-export default SignUp;
+export default SignIn;
